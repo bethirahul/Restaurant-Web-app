919ee1c16e10eace0bf7401484099f1dae3550b5
Rahul Bethi <rahulbethi@hotmail.com>
2018-02-16 03:25:20 -0800

    Seperated database setup file and main file
    added os environment port
    added try except method to handle exceptions

--- crud.py
+++ crud.py
@@ -1,34 +1,9 @@
 #!/usr/bin/env python3
 
-# Functions and variables used to manipulate different parts of python runtime environment
-import sys
-
-# SQLAlchemy variables used in writing mapper code
-from sqlalchemy import Column, ForeignKey, Integer, String
-
-# To use in configuration and class code
-from sqlalchemy.ext.declarative import declarative_base
-
-# To relate Foreign Key relationships
-from sqlalchemy.orm import relationship
-
-# To use in the configuration code at the end of configuration
-from sqlalchemy import create_engine
-
-# Basic HTTP server
+import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
-# Tools for parsing the incoming messages from clients
 from urllib.parse import unquote, parse_qs
-
-# Tools for threading, used to recieve and handle multiple requests at once
-#import threading
 from socketserver import ThreadingMixIn
-
-
-# Instance of the declarative base class
-Base = declarative_base()
-# ^^Will let the SQLAlchemy know that out classes are special SQLAlchemy classes that correspond to the tables in our database
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
@@ -52,10 +27,11 @@ class MessageHandler(BaseHTTPRequestHandler):
     """To handle messages from the client"""
     def do_GET(self):
 
-        name = unquote(self.path[1:])
-        print('Unquoted Path: {}'.format(name))
         try:
+            name = unquote(self.path[1:])
+            print('Unquoted Path: {}'.format(name))
             if name == '':
+
                 # send a 200 OK response
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html; charset=utf-8')
@@ -64,8 +40,9 @@ class MessageHandler(BaseHTTPRequestHandler):
                 # Writing message
                 mesg = form.format("\n".join(memory))
                 self.wfile.write(mesg.encode())
-
-                return
+            
+            elif name == 'restaurants':
+                restaurants = session.query(Restaurant).all()
         
         except IOError:
             self.send_response(404)
@@ -75,29 +52,34 @@ class MessageHandler(BaseHTTPRequestHandler):
             self.wfile.write("404: Not found".encode())
 
     def do_POST(self):
-        # Getting message length
-        length = int(self.headers.get('Content-length', 0))
+        try:
+            # Send a 303 back to the root page
+            self.send_response(303)  # redirect via GET
+            self.send_header('Location', '/')
+            self.end_headers()
 
-        # Read and parse message
-        data = self.rfile.read(length).decode()
-        message = parse_qs(data)['message'][0]
+            # Getting message length
+            length = int(self.headers.get('Content-length', 0))
 
-        # Escape HTML tags in the message so users can't break world+dog.
-        message = message.replace("<", "&lt;")
+            # Read and parse message
+            data = self.rfile.read(length).decode()
+            message = parse_qs(data)['message'][0]
 
-        # Store it in memory.
-        memory.append(message)
+            # Escape HTML tags in the message so users can't break world+dog.
+            message = message.replace("<", "&lt;")
 
-        # Send a 303 back to the root page
-        self.send_response(303)  # redirect via GET
-        self.send_header('Location', '/')
-        self.end_headers()
+            # Store it in memory.
+            if message != '':
+                memory.append(message)
+        
+        except:
+            pass
 
 
 # This starts the server
 if __name__ == '__main__':
     try:
-        port = 8000
+        port = int(os.environ.get('PORT', 8000))
         server_address = ('', port)
         server = ThreadedHTTPServer(server_address, MessageHandler)
         print("\nServer is up and running on port {} >>>>>>>>>>\n".format(port))
@@ -107,19 +89,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("\n^^ Keyboard exception received, stopping server...\n")
         server.socket.close()
-
-
-
-    
-
-
-
-
-########## At End of File ##########
-
-# Instance of create engine class and point to database we use
-## engine = create_engine('sqlite:///restaurants.db')
-# ^^Above example has SQLite3 with database 'restaurant'
-
-# This goes into database and creates all the classes we will soon create as new tables
-## Base.metadata.create_all(engine)
---
+++ database_setup.py
@@ -0,0 +1,44 @@
+# Functions and variables used to manipulate different parts of python runtime
+#   environment
+import sys
+
+# SQLAlchemy variables used in writing mapper code
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+# To use in configuration and class code
+from sqlalchemy.ext.declarative import declarative_base
+
+# To relate Foreign Key relationships
+from sqlalchemy.orm import relationship
+
+# To use in the configuration code at the end of configuration
+from sqlalchemy import create_engine
+
+
+# Instance of the declarative base class
+Base = declarative_base()
+# ^^ Will let the SQLAlchemy know that out classes are special SQLAlchemy
+#   classes that correspond to the tables in our database
+
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
