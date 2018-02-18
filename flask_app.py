f5ce8be6a642fd8cd0778fc967c7d1bc465560b4
Rahul Bethi <rahulbethi@hotmail.com>
2018-02-18 04:15:23 -0800

    Implemented all GET requests
    Including Add, Edit, Delete for items

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -15,62 +15,252 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-root_path = '/'
-all_res_path = root_path + 'restaurants/'
-add_res_path = all_res_path + 'add/'
-res_path = all_res_path + '{}/'
-edit_res_path = res_path + 'edit/'
-del_res_path = res_path + 'delete/'
+# Home Page
+root_path = '/' ##
+## Restaurants
+all_res_path = root_path + 'restaurants/' ##
+add_res_path = all_res_path + 'add/' #
+add_res_e_path = add_res_path + 'error/' #
+### Needs Restaurant ID
+res_path = all_res_path + '{res_id}/' ##
+edit_res_path = res_path + 'edit/' #
+edit_res_e_path = edit_res_path + 'error/' #
+del_res_path = res_path + 'delete/' #
+add_item_path = res_path + 'add/' #
+add_item_e_path = add_item_path + 'error/' #
+#### Needs Restaurant ID and Item ID
+edit_item_path = res_path + '{item_id}-edit/' #
+edit_item_e_path = edit_item_path + 'error/' #
+del_item_path = res_path + '{item_id}-delete/' #
 
 
 # These must be placed after setting database and Flask name to flask app
-@app.route(root_path)
+
+#========================
+# Root - Home Page
+@app.route(root_path, methods=['GET'])
 def homePage():
+    '''Home Page'''
     return html_data.rt_cnt.format(all_res_path=all_res_path)
 
-@app.route(all_res_path)
-def showAllRestaurants():
+
+#========================
+# All Restaurants Page
+@app.route(all_res_path, methods=['GET'])
+def AllRestaurantsPage():
+    '''All Restaurants Page'''
     # Get all restaurants
     all_res = session.query(Restaurant)
     if all_res:
         output_html = ''
         for res in all_res:
-            output_html += html_data.res_cnt.format(
-                    res_path = res_path.format(str(res.id)),
+            output_html += html_data.res_link_cnt.format(
+                    res_path = res_path.format(res_id = str(res.id)),
                     res_name = res.name,
-                    edit_res_path = edit_res_path.format(str(res.id)),
-                    del_res_path = del_res_path.format(str(res.id))
-                )
+                    edit_res_path = edit_res_path.format(res_id = str(res.id)),
+                    del_res_path = del_res_path.format(res_id = str(res.id))
+                    )
         
-        return html_data.res_cnt.format(
+        return html_data.all_res_cnt.format(
                 add_res_path=add_res_path,
                 all_res_cnt=output_html
-            )
+                )
     
-    return 'SQL Error: Unable to fetch restaurants!'
+    return redirect(root_path)
 
-@app.route(add_res_path)
-def addRestaurant():
-    # Add new restaurant
-    res = Restaurant
 
-@app.route(res_path.format('<int:res_id>'))
-def restaurantMenu(res_id):
+#========================
+# Add Restaurant Page
+@app.route(add_res_path, methods=['GET', 'POST'])
+@app.route(add_res_e_path, methods=['GET', 'POST'])
+def addRestaurantPage():
+    '''Add Restaurant Page'''
+    if request.method == 'GET':
+        error_html = ''
+        if request.path == add_res_e_path:
+            error_html = html_data.res_e_cnt
+        
+        return html_data.add_res_cnt.format(
+                all_res_path = all_res_path,
+                error = error_html
+                )
+
+
+#========================
+# Each Restaurant Page
+@app.route(res_path.format(res_id = '<int:res_id>'), methods=['GET'])
+def restaurantPage(res_id):
+    '''Restaurant page with items in it'''
     # Get restaurant
     res = session.query(Restaurant).filter_by(id = res_id).one()
-    output_text = res.name + '<br><br>'
 
-    # Get the restaurant's menu items
-    items = session.query(MenuItem).filter_by(restaurant_id=res.id)
-    for item in items:
-        output_text += item.name + '<br>'
-        output_text += item.price + '<br>'
-        output_text += item.description + '<br>'
-        output_text += '<br>'
+    if res:
+        output_html = ''
+        # Get the restaurant's menu items
+        items = session.query(MenuItem).filter_by(restaurant_id=res.id)
+        for item in items:
+            output_html += html_data.res_item_cnt.format(
+                    item_name = item.name,
+                    item_price = item.price,
+                    item_desc = item.description,
+                    edit_item_path = edit_item_path.format(
+                            res_id = res_id,
+                            item_id = item.id
+                            ),
+                    del_item_path = del_item_path.format(
+                            res_id = res_id,
+                            item_id = item.id
+                            )
+                    )
 
-    return output_text
+        return html_data.res_cnt.format(
+                res_name = res.name,
+                all_res_path = all_res_path,
+                add_item_path = add_item_path.format(res_id = res.id),
+                res_items = output_html
+                )
+    
+    return redirect(all_res_path)
 
 
+#========================
+# Edit Restaurant Page
+@app.route(
+    edit_res_path.format(res_id = '<int:res_id>'), methods=['GET', 'POST'])
+@app.route(
+    edit_res_e_path.format(res_id = '<int:res_id>'), methods=['GET', 'POST'])
+def editRestaurantPage(res_id):
+    '''Edit Restaurant Page'''
+    # Get restaurant
+    res = session.query(Restaurant).filter_by(id = res_id).one()
+
+    if request.method == 'GET' and res:
+        error_html = ''
+        if request.path == edit_res_e_path.format(res_id = res_id):
+            error_html = html_data.res_e_cnt
+        
+        return html_data.edit_res_cnt.format(
+                res_name = res.name,
+                all_res_path = all_res_path,
+                error = error_html
+                )
+    
+    return redirect(all_res_path)
+
+
+#========================
+# Delete Restaurant Page
+@app.route(
+    del_res_path.format(res_id = '<int:res_id>'), methods=['GET', 'POST'])
+def deleteRestaurantPage(res_id):
+    '''Delete Restaurant Page'''
+    # Get restaurant
+    res = session.query(Restaurant).filter_by(id = res_id).one()
+
+    if request.method == 'GET' and res:
+        return html_data.del_res_cnt.format(
+                res_name = res.name,
+                all_res_path = all_res_path
+                )
+
+    return redirect(all_res_path)
+
+
+#========================
+# Add Item Page
+@app.route(
+    add_item_path.format(res_id = '<int:res_id>'), methods=['GET', 'POST'])
+@app.route(
+    add_item_e_path.format(res_id = '<int:res_id>'), methods=['GET', 'POST'])
+def addItemPage(res_id):
+    '''Add Item Page'''
+    # Get restaurant
+    res = session.query(Restaurant).filter_by(id = res_id).one()
+
+    if request.method == 'GET' and res:
+        error_html = ''
+        if request.path == add_item_e_path.format(res_id = res_id):
+            error_html = html_data.item_e_cnt
+        
+        return html_data.add_item_cnt.format(
+                res_name = res.name,
+                res_path = res_path.format(res_id = res_id),
+                error = error_html
+                )
+
+    return redirect(all_res_path)
+
+
+#========================
+# Edit Item Page
+@app.route(
+    edit_item_path.format(
+        res_id = '<int:res_id>',
+        item_id = '<int:item_id>'),
+        methods=['GET', 'POST']
+        )
+@app.route(
+    edit_item_e_path.format(
+        res_id = '<int:res_id>',
+        item_id = '<int:item_id>'),
+        methods=['GET', 'POST']
+        )
+def editItemPage(res_id, item_id):
+    '''Edit Item Page'''
+    # Get restaurant
+    res = session.query(Restaurant).filter_by(id = res_id).one()
+    if res:
+        item = session.query(MenuItem).filter_by(
+                id = item_id,
+                restaurant_id = res_id
+                ).one()
+
+        if request.method == 'GET' and item:
+            error_html = ''
+            if request.path == edit_item_e_path.format(
+                    res_id = res_id,
+                    item_id = item_id
+                ):
+                error_html = html_data.item_e_cnt
+            
+            return html_data.edit_item_cnt.format(
+                    item_name = item.name,
+                    res_name = res.name,
+                    res_path = res_path.format(res_id = res_id),
+                    item_price = item.price,
+                    item_desc = item.description,
+                    error = error_html
+                    )
+
+    return redirect(all_res_path)
+
+
+#========================
+# Delete Item Page
+@app.route(
+    del_item_path.format(
+        res_id = '<int:res_id>',
+        item_id = '<int:item_id>'),
+        methods=['GET', 'POST']
+        )
+def deleteItemPage(res_id, item_id):
+    '''Delete Item Page'''
+    # Get restaurant
+    res = session.query(Restaurant).filter_by(id = res_id).one()
+    if res:
+        item = session.query(MenuItem).filter_by(
+                id = item_id,
+                restaurant_id = res_id
+                ).one()
+
+        if request.method == 'GET' and item:
+            return html_data.del_item_cnt.format(
+                    item_name = item.name,
+                    res_name = res.name,
+                    res_path = res_path.format(res_id = res_id)
+                )
+
+    return redirect(all_res_path)
 
 
 if __name__ == '__main__':
--- html_data.py
+++ html_data.py
@@ -2,8 +2,7 @@ root_cnt = '''<!DOCTYPE html>
   <title>Message Board</title>
   <a href="{0}">Show Restaurants</a><br><br>
   <form method="POST">
-    <textarea name="message"></textarea>
-    <br>
+    <textarea name="message"></textarea><br>
     <button type="submit">Post it!</button>
   </form>
   <pre>
@@ -12,6 +11,8 @@ root_cnt = '''<!DOCTYPE html>
 </html>
 '''
 
+#========================
+# Root - Home Page
 rt_cnt = '''<!DOCTYPE html>
   <title>Restaurants - Home Page</title>
   <body>
@@ -21,72 +22,189 @@ rt_cnt = '''<!DOCTYPE html>
 </html>
 '''
 
+#========================
+# All Restaurants Page
 all_res_cnt = '''<!DOCTYPE html>
   <title>Restaurants</title>
   <body>
-    <h1>Restaurants:</h1>
-    <br>
+    <h1>Restaurants:</h1><br>
     <a href="{add_res_path}">Add new Restaurant</a>
-    <br>
-    <br>
-    {all_res_cnt}
+{all_res_cnt}
   </body>
 </html>
 '''
-
-res_cnt = '''    <p>
-      <a href="{res_path}">{res_name}</a>
-    </p>
+# each restaurant name with edit and delete
+res_link_cnt = '''    <br><br>
+    <a href="{res_path}">{res_name}</a><br>
     <a href="{edit_res_path}">Edit</a>
     <a href="{del_res_path}">Delete</a>
-    <br>
-    <br>
 '''
 
+#========================
+# Each Restaurant Page
+res_cnt = '''<!DOCTYPE html>
+  <title>{res_name}</title>
+  <body>
+    <h3>{res_name}</h3>
+    <a href="{all_res_path}">Go back</a><br>
+    <br>
+    <a href="{add_item_path}">Add Item</a><br>
+    <br>
+{res_items}
+  </body>
+</html>
+'''
+# each item in Restaurant page
+res_item_cnt = '''    <p>
+      {item_name}<br>
+      {item_price}<br>
+      {item_desc}
+    </p>
+    <a href="{edit_item_path}">Edit</a>
+    <a href="{del_item_path}">Delete</a>
+'''
+
+#========================
+# Add Restaurant Page
 add_res_cnt = '''<!DOCTYPE html>
   <title>Add Restaurant</title>
   <body>
-    <h3>Add a new Restaurant</h3><br>
+    <h3>Add a new Restaurant</h3>
+    <a href="{all_res_path}">Go back</a><br>
+    <br>
     <form method="POST">
       <input
         type="text"
         placeholder="Restaurant name"
         name="restaurant_name">
       <button type="submit">Add</button>
-    </form>{0}<br>
-    <a href="{1}">Go back</a>
+    </form>{error}
   </body>
 </html>
 '''
-
+# field empty error
 res_e_cnt = '''
     <p>Please fill in the restaurant name!</p>
 '''
 
+#========================
+# Edit Restaurant Page
 edit_res_cnt = '''<!DOCTYPE html>
   <title>Edit Restaurant</title>
   <body>
-    <h4>Edit restaurant \'{0}\'</h4>
+    <h4>Edit restaurant \'{res_name}\'</h4>
+    <a href="{all_res_path}">Go back</a><br>
+    <br>
     <form method="POST">
       <input
         type="text"
         placeholder="New restaurant name"
         name="restaurant_name">
       <button type="submit">Update</button>
-    </form>{1}<br>
-    <a href="{2}">Go back</a>
+    </form>{error}
   </body>
 </html>
 '''
 
+#========================
+# Delete Restaurant Page
 del_res_cnt = '''<!DOCTYPE html>
-  <title>Edit Restaurant</title>
+  <title>Delete Restaurant</title>
   <body>
-    <h4>Are you sure you want to delete restaurant \'{0}\'?</h4>
+    <h4>Are you sure you want to delete restaurant \'{res_name}\'?</h4>
     <form method="POST">
       <input type="submit" value="Delete">
     </form><br>
-    <a href="{1}">Go back</a>
+    <a href="{all_res_path}">Go back</a>
+  </body>
+</html>
+'''
+
+#========================
+# Add Item Page
+add_item_cnt = '''<!DOCTYPE html>
+  <title>Add Item</title>
+  <body>
+    <h3>Add a new Item in Restaurant \'{res_name}\'</h3>
+    <a href="{res_path}">Go back</a><br>
+    <br>
+    <form method="POST">
+      Name:
+      <input
+        type="text"
+        placeholder="Item name"
+        name="item_name"><br>
+      <br>
+      Price: $
+      <input
+        type="text"
+        placeholder="Item price"
+        name="item_price"><br>
+      <br>
+      Description:<br>
+      <textarea
+        placeholder="Item Description"
+        name="message">
+      </textarea><br>
+      <button type="submit">Add</button>
+    </form>{error}
+  </body>
+</html>
+'''
+# field empty error
+item_e_cnt = '''
+    <p>Error: One of the fields is empty!</p>
+'''
+
+#========================
+# Edit Item Page
+edit_item_cnt = '''<!DOCTYPE html>
+  <title>Edit Item</title>
+  <body>
+    <h3>Edit \'{item_name}\' in restaurant \'{res_name}\'</h3>
+    <a href="{res_path}">Go back</a><br>
+    <br>
+    <h4>Present Name, Price and Description:</h4>
+    <p>
+      {item_name}<br>
+      {item_price}<br>
+      {item_desc}
+    </p>
+    <h4>Please update with new values below:</h4>
+    <form method="POST">
+      Name:
+      <input
+        type="text"
+        placeholder="Item name"
+        name="item_name"><br>
+      <br>
+      Price: $
+      <input
+        type="text"
+        placeholder="Item price"
+        name="item_price"><br>
+      <br>
+      Description:<br>
+      <textarea
+        placeholder="Item Description"
+        name="message">
+      </textarea><br>
+      <button type="submit">Update</button>
+    </form>{error}
+  </body>
+</html>
+'''
+
+#========================
+# Delete Restaurant Page
+del_item_cnt = '''<!DOCTYPE html>
+  <title>Delete Item</title>
+  <body>
+    <h4>Are you sure you want to delete item \'{item_name}\' from restaurant \'{res_name}\'?</h4>
+    <form method="POST">
+      <input type="submit" value="Delete">
+    </form><br>
+    <a href="{res_path}">Go back</a>
   </body>
 </html>
 '''
